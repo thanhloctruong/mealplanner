2da25b891cfa927d7a341203253efe7c022c1bbf
ACan <thanhloctruong102@gmail.com>
2021-10-14 14:54:29 +0700

    push heroku

--- backend/server.js
+++ backend/server.js
@@ -1,11 +1,13 @@
 import express from "express";
 import mongoose from "mongoose";
+import path from "path";
 
 import userRouter from "./router/UserRouter.js";
 
 const app = express();
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
+const __dirname = path.resolve();
 
 mongoose.connect(
   process.env.MONGODB_URL ||
@@ -17,9 +19,13 @@ mongoose.connect(
 );
 app.use("/api/users", userRouter);
 
-app.get("/", (req, res) => {
-  res.send("server is already");
-});
+app.use(express.static(path.join(__dirname, "/meal-planner/build")));
+app.get("*", (req, res) =>
+  res.sendFile(path.join(__dirname, "/meal-planner/build/index.html"))
+);
+// app.get("/", (req, res) => {
+//   res.send("server is already");
+// });
 const port = process.env.PORT || 5000;
 app.listen(port, () => {
   console.log(`Serve at http://localhost:${port}`);
