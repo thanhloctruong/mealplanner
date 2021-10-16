dfa41247ae00881d2bc063d9d9e53405e56d6e28
ACan <thanhloctruong102@gmail.com>
2021-10-16 11:30:53 +0700

    pkg

--- backend/server.js
+++ backend/server.js
@@ -25,9 +25,9 @@ app.get("*", (req, res) =>
 );
 
 // aaa
-// app.get("/", (req, res) => {
-//   res.send("server is already");
-// });
+app.get("/", (req, res) => {
+  res.send("server is already");
+});
 const port = process.env.PORT || 5000;
 app.listen(port, () => {
   console.log(`Serve at http://localhost:${port}`);
