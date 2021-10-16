bb7f4515cf532f5352f2473475d198b726c3b5c3
ACan <thanhloctruong102@gmail.com>
2021-10-16 11:38:53 +0700

    pkg

--- backend/server.js
+++ backend/server.js
@@ -23,12 +23,11 @@ app.use(express.static(path.join(__dirname, "/meal-planner/build")));
 app.get("*", (req, res) =>
   res.sendFile(path.join(__dirname, "/meal-planner/build/index.html"))
 );
-
 // aaa
-app.get("/", (req, res) => {
-  res.send("server is already");
-});
-const port = process.env.PORT || 5000;
-app.listen(port, () => {
-  console.log(`Serve at http://localhost:${port}`);
+// app.get("/", (req, res) => {
+//   res.send("server is already");
+// });s
+const PORT = process.env.PORT || 5000;
+app.listen(PORT, () => {
+  console.log(`Serve at http://localhost:${PORT}`);
 });
